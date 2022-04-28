f09540fa5db672bbda0afdd58e3fd343951a8a44
GrandMaster5000 <ggamer556@mail.ru>
2022-04-28 21:40:33 +0300

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { Header } from '../components/Header';
+import { LeftMenu } from '../components/LeftMenu';
+import { Post } from '../components/Post';
+import { SideComments } from '../components/SideComments';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="wrapper">
+        <div className="leftSide">
+          <LeftMenu />
+        </div>
+        <div className="content">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+        <div className="rightSide">
+          <SideComments />
+        </div>
+      </div>
+    </div>
+  );
+}
